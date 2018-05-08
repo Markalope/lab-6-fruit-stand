9b02716f69efa65215db5a0fbb63c0cb8b43db7c
Mark Myers <markalope@gmail.com>
2018-05-08 15:37:13 -0700

    finalized and tested my array of objects in fruit.js

--- js/fruit.js
+++ js/fruit.js
@@ -1,77 +1,81 @@
 console.log('fruit.js is loading');
 /* exported apple strawberry pomegranate dragonfruit orange peach banana lemon yellowApple */
 
-//red fruits
-var apple = {
-    name: 'Red Apple',
-    price: '$1.99/lb',
-    description: 'Keeps doctor away.',
-    color: 'red',
-    image: 'apple.png',
-};
+var allFruits = [
 
-var strawberry = {
-    name: 'Strawberry',
-    price: '$.99/lb',
-    description: 'Sweet and Seedy.',
-    color: 'red',
-    image: 'strawberry.png',
-};
+    {
+        name: 'Red Apple',
+        price: '$1.99/lb',
+        desc: 'Keeps doctor away.',
+        color: 'red',
+        image: 'apple.png'
+    },
+    
+    {
+        name: 'Strawberry',
+        price: '$.99/lb',
+        description: 'Sweet and Seedy.',
+        color: 'red',
+        image: 'strawberry.png'
+    },
 
-var pomegranate = {
-    name: 'Pomegranate',
-    price: '$2.29',
-    description: 'Sweet. Seed-filled. Biggish.',
-    color: 'red',
-    image: 'pomegranate.png',
-};
-
-var dragonfruit = {
-    name: 'Dragonfruit',
-    price: '$3.99',
-    description: 'OK it\s more pink than red. Sue me.',
-    color: 'red',
-    image: 'dragonfruit.png',
-};
+    {
+        name: 'Pomegranate',
+        price: '$2.29',
+        description: 'Sweet. Seed-filled. Biggish.',
+        color: 'red',
+        image: 'pomegranate.png'
+    },
+    {
+        name: 'Dragonfruit',
+        price: '$3.99',
+        description: 'OK it\s more pink than red. Sue me.',
+        color: 'red',
+        image: 'dragonfruit.png'
+    },
 
 //orange fruits
-var orange = {
-    name: 'Orange',
-    price: '$2.00/lb',
-    description: 'They\re not just for breakfast anymore.',
-    color: 'orange',
-    image: 'orange.png',
-};
-
-var peach = {
-    name: 'Orange',
-    price: '$2.79/lb',
-    description: 'Dude. Seriously. Peaches are not orange.',
-    color: 'orange',
-    image: 'peach.png',
-};
+    {
+        name: 'Orange',
+        price: '$2.00/lb',
+        description: 'They\re not just for breakfast anymore.',
+        color: 'orange',
+        image: 'orange.png',
+    },
+    {
+        name: 'Orange',
+        price: '$2.79/lb',
+        description: 'Dude. Seriously. Peaches are not orange.',
+        color: 'orange',
+        image: 'peach.png',
+    },
 
 //yellow fruits
-var banana = {
-    name: 'Banana',
-    price: '$1.79/lb',
-    description: 'Can you tell I have no idea what fruit costs?',
-    color: 'yellow',
-    image: 'banana.png',
-};
+    {
+        name: 'Banana',
+        price: '$1.79/lb',
+        description: 'Can you tell I have no idea what fruit costs?',
+        color: 'yellow',
+        image: 'banana.png',
+    },
 
-var lemon = {
-    name: 'Lemon',
-    price: '$.79/lb',
-    description: 'Used for Lemonade and Iced Tea. Hey, let\s mix these up!',
-    color: 'yellow',
-    image: 'lemon.png',
-};
+    {
+        name: 'Lemon',
+        price: '$.79/lb',
+        description: 'Used for Lemonade and Iced Tea. Hey, let\s mix these up!',
+        color: 'yellow',
+        image: 'lemon.png'
+    },
 
-var yellowApple = {
-    name: 'Yellow Apple',
-    price: '$2.79/lb',
-    description: 'These are for baking mostly, right?',
-    color: 'yellow',
-    image: 'yellowapple.png',
-};
+    {
+        name: 'Yellow Apple',
+        price: '$2.79/lb',
+        description: 'These are for baking mostly, right?',
+        color: 'yellow',
+        image: 'yellowapple.png'
+    }
+
+];
+
+console.log(allFruits[1].name);
+console.log(allFruits[1]);
