755dac41d16151cf07b05a1cb428bc9cdd5fb94d
Mark Myers <markalope@gmail.com>
2018-05-08 16:57:04 -0700

    got th3e images inserting into the html

--- js/app.js
+++ js/app.js
@@ -6,11 +6,18 @@ var position = document.getElementById('fruit-list');
 
 // This loop is for testing to see if my array is working properly
 for(var i = 0; i < allFruits.length; i++){
-    console.log(allFruits[i].description);
-    document.write('<img src="./images/' + allFruits[i].image + '" width = "100">');
     
     var li = allFruits[i].createLi();
 
     position.appendChild(li);
+
+    const imgEle = document.createElement('img');
+    imgEle.width = 100;
+    imgEle.src = './images/' + allFruits[i].image;
+
+console.log(imgEle);
+
+position.appendChild(imgEle);
+
 }
 
