1742ba89a1390a438070dc2d6302c368ef8d7e9c
Mark Myers <markalope@gmail.com>
2018-05-08 14:02:14 -0700

    created image folder and added images, created placeholders in HTML file

--- js/fruit.js
+++ js/fruit.js
@@ -1,1 +1,16 @@
 console.log('fruit.js is loading');
+
+//red fruits
+'apple'
+'strawberry'
+'pomegranate'
+'dragonfruit'
+
+//orange fruits
+'orange'
+'peach'
+
+//yellow fruits
+'banana'
+'lemon'
+'yellow apple'
